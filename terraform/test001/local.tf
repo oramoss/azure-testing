9f499e404aba4e781446cfb5db3934b836c40c61
jeff.moss <jeff.moss@oramoss.com>
2025-03-13 10:28:24 +0000

    Amend rg name to include region test001 tf

--- terraform/test001/local.tf
+++ terraform/test001/local.tf
@@ -1,7 +1,13 @@
 locals {
+  location_abbreviated_map = {
+    uksouth = "uks"
+    ukwest  = "ukw"
+  }
+
   default_audience_name = "api://AzureADTokenExchange"
   github_issuer_url     = "https://token.actions.githubusercontent.com"
   label                 = var.environment
+  location_abbreviated  = lookup(local.location_abbreviated_map, var.location, null)
   subscription_id       = var.subscription_id
   tags                  = merge(var.global_tags, var.environment_tags)
 }
--- terraform/test001/main.tf
+++ terraform/test001/main.tf
@@ -1,6 +1,6 @@
 resource "azurerm_resource_group" "rgs" {
   for_each = { for my_rg in var.resource_groups : my_rg.name => my_rg }
-  name     = format("%s-%s", local.label, each.value.name)
+  name     = format("%s-%s-%s", local.label, local.location_abbreviated, each.value.name)
   location = var.location
 
   tags = merge(local.tags, { "Deployment Date" = formatdate("YYYY-MM-DD hh:mm ZZZ", timestamp()) })
